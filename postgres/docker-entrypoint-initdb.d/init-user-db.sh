f9a4b85e40270756faa5de118c09467be32f64ca
Marvin Bredal Lillehaug <marvin.lillehaug@kantega.no>
2017-04-08 11:32:35 +0200

    devnull

--- postgres/docker-entrypoint-initdb.d/init-user-db.sh
+++ postgres/docker-entrypoint-initdb.d/init-user-db.sh
@@ -5,4 +5,8 @@ psql -v ON_ERROR_STOP=1 --username "$POSTGRES_USER" <<-EOSQL
     CREATE USER ems WITH PASSWORD 'ems';
     CREATE DATABASE ems;
     GRANT ALL PRIVILEGES ON DATABASE ems TO ems;
+
+    CREATE USER devnull WITH PASSWORD 'devnull';
+    CREATE DATABASE devnull;
+    GRANT ALL PRIVILEGES ON DATABASE devnull TO devnull;
 EOSQL
